b241f0575377973025ea9f411e5c3d9316e3d79d
ispan <ispan@DESKTOP-PAKSETB>
2022-10-26 17:41:34 +0800

    加入專案檔案。

---
+++ Exec2_Message/Form1.Designer.cs
@@ -0,0 +1,75 @@
+﻿namespace Exec2_Message
+{
+	partial class Form1
+	{
+		/// <summary>
+		/// 設計工具所需的變數。
+		/// </summary>
+		private System.ComponentModel.IContainer components = null;
+
+		/// <summary>
+		/// 清除任何使用中的資源。
+		/// </summary>
+		/// <param name="disposing">如果應該處置受控資源則為 true，否則為 false。</param>
+		protected override void Dispose(bool disposing)
+		{
+			if (disposing && (components != null))
+			{
+				components.Dispose();
+			}
+			base.Dispose(disposing);
+		}
+
+		#region Windows Form 設計工具產生的程式碼
+
+		/// <summary>
+		/// 此為設計工具支援所需的方法 - 請勿使用程式碼編輯器修改
+		/// 這個方法的內容。
+		/// </summary>
+		private void InitializeComponent()
+		{
+			this.timeButton = new System.Windows.Forms.Button();
+			this.timeLabel = new System.Windows.Forms.Label();
+			this.SuspendLayout();
+			// 
+			// timeButton
+			// 
+			this.timeButton.Location = new System.Drawing.Point(71, 50);
+			this.timeButton.Name = "timeButton";
+			this.timeButton.Size = new System.Drawing.Size(75, 23);
+			this.timeButton.TabIndex = 0;
+			this.timeButton.Text = "顯示時間";
+			this.timeButton.UseVisualStyleBackColor = true;
+			this.timeButton.Click += new System.EventHandler(this.timeButton_Click);
+			// 
+			// timeLabel
+			// 
+			this.timeLabel.AutoSize = true;
+			this.timeLabel.Location = new System.Drawing.Point(191, 55);
+			this.timeLabel.Name = "timeLabel";
+			this.timeLabel.Size = new System.Drawing.Size(25, 12);
+			this.timeLabel.TabIndex = 1;
+			this.timeLabel.Text = "time";
+			this.timeLabel.Click += new System.EventHandler(this.timeLabel_Click);
+			// 
+			// Form1
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.ClientSize = new System.Drawing.Size(800, 450);
+			this.Controls.Add(this.timeLabel);
+			this.Controls.Add(this.timeButton);
+			this.Name = "Form1";
+			this.Text = "Form1";
+			this.ResumeLayout(false);
+			this.PerformLayout();
+
+		}
+
+		#endregion
+
+		private System.Windows.Forms.Button timeButton;
+		private System.Windows.Forms.Label timeLabel;
+	}
+}
+
---
+++ Exec2_Message/Form1.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Exec2_Message
+{
+	public partial class Form1 : Form
+	{
+		public Form1()
+		{
+			InitializeComponent();
+		}
+
+		private void timeButton_Click(object sender, EventArgs e)
+		{
+			
+			DateTime now = DateTime.Now;
+			string dt = DateTime.Now.ToString();
+			
+
+			//分辨早安午安晚安
+
+			string date = TransTime(now);
+
+
+			//輸出現在時間
+			timeLabel.Text = dt + "\r\n" + date;
+
+			MessageBox.Show(DateTime.Now.ToString("yyyy/MM/dd HH:mm:ss"));
+		}
+
+		private string TransTime(DateTime date)
+		{ 
+			int hour = date.Hour;
+			string talk = string.Empty;
+			if (hour >= 6 && hour < 12)
+			{
+				talk = "早安";
+			}
+			else if (hour >= 12 && hour < 18)
+			{
+				talk = "午安";
+			}
+			else
+			{
+				talk = "晚安";
+			}
+			return talk;
+		}
+
+		private void timeLabel_Click(object sender, EventArgs e)
+		{
+			
+		}
+
+		
+	}
+
+}
+  
